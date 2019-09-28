96dae7720f045eb96b52333bdab5e1c50c2305e4
Hasan Abdullah <hasan.cse91@gmail.com>
2019-09-28 23:37:59 +0600

    project done

--- app/src/main/java/com/hellohasan/recyclerviewmultipleviewtype/MainActivity.kt
+++ app/src/main/java/com/hellohasan/recyclerviewmultipleviewtype/MainActivity.kt
@@ -22,12 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun getPostDataList() : List<PostData> {
 
-        val postList: List<PostData> = listOf()
+        val postList = mutableListOf<PostData>()
 
         for (i in 1..5) {
-            val postData1 = PostData(userName = "John Doe", timeStamp = "10:23 PM, 23 Sep 2019", userProfilePhotoUrl = "", postDescription = "", postImageUrl = "")
-            val postData2 = PostData(userName = "Grayson Hammond", timeStamp = "9:52 AM, 20 Sep 2019", userProfilePhotoUrl = "", postDescription = "", postImageUrl = "")
-            val postData3 = PostData(userName = "Riley Clark", timeStamp = "1:49 PM, 9 Sep 2019", userProfilePhotoUrl = "", postDescription = "", postImageUrl = "")
+            val postData1 = PostData(userName = "John Doe", timeStamp = "10:23 PM, 23 Sep 2019", userProfilePhotoUrl = "https://i.imgur.com/L3J7Oge.jpg", postDescription = "This is my first post on this platform. I have uploaded this awesome photo for your opinion. Have a good day", postImageUrl = "https://i.imgur.com/bZDLmPw.jpg")
+            val postData2 = PostData(userName = "Grayson Hammond", timeStamp = "9:52 AM, 20 Sep 2019", userProfilePhotoUrl = "https://i.imgur.com/zmJP4JM.png", postDescription = "It is better to lead from behind and to put others in front, especially when you celebrate victory when nice things occur. You take the front line when there is danger. Then people will appreciate your leadership. -[Nelson Mandela]", postImageUrl = "")
+            val postData3 = PostData(userName = "Riley Clark", timeStamp = "1:49 PM, 9 Sep 2019", userProfilePhotoUrl = "https://i.imgur.com/KvKTla7.png", postDescription = "It's coffee time!", postImageUrl = "https://i.imgur.com/L0X8CWb.jpg")
+
+            postList.add(postData1)
+            postList.add(postData2)
+            postList.add(postData3)
         }
+
+        return postList
     }
 }
